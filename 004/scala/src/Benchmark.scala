6be35a1cde23a2592d928a306cbbd080fc30fe7c
Andrew Watson <aww@awwsmm.com>
2020-03-22 13:39:10 +0000

    final benchmarks?

--- 004/scala/src/Benchmark.scala
+++ 004/scala/src/Benchmark.scala
@@ -1,11 +1,16 @@
 import scala.util.control.Breaks._
 import scala.util.Random
 import org.scalameter.api._
+import org.scalameter.picklers.Implicits._
 
 // see Scaladoc at
 //   https://www.javadoc.io/doc/com.storm-enroute/scalameter_2.13/latest/org/scalameter/index.html
 
-object Benchmark extends Bench.OfflineReport {
+// at the moment, multiple benchmarks cannot be run with Bench.OfflineReport
+//   https://github.com/scalameter/scalameter/issues/159
+// ...so it's best to just put everything into a single benchmark object
+
+object DCP004 {
 
   private val rand = new Random
 
@@ -15,7 +20,7 @@ object Benchmark extends Bench.OfflineReport {
     else if (fracBad == 1.0) Array.fill(n)(0)
     else {
       val maxVal = n * maxScaling
-      val minVal = 1 - fracBad / (1 - fracBad) * maxVal
+      val minVal = 0.5 - fracBad / (1 - fracBad) * maxVal
 
       if (minVal == maxVal) Array.fill(n)(minVal.toInt)
       else (1 to n).map(_ =>
@@ -94,30 +99,79 @@ object Benchmark extends Bench.OfflineReport {
     }
   }
 
-  val lengths = Gen.exponential("n")(10, 10000, 2)
-  //  val fracBad = Gen.range("fracBad")(0, 1, 0.05)
-  //  val maxScaling = Gen.range("maxScaling")(0.1, 1.25, 0.05)
+}
 
-  val arrays: Gen[IndexedSeq[Array[Int]]] = for {
-    length <- lengths
-  } yield (1 until length).map(e => randArray(e, 0, 1))
+object Benchmark extends Bench.OfflineReport {
+  import DCP004._
 
-  performance of "Benchmark" in {
-    measure method "findMissingMutable" in {
-      using(arrays) in {
-        arr => arr.foreach(findMissingMutable)
-      }
-    }
-    measure method "findMissingInPlace" in {
-      using(arrays) in {
-        arr => arr.foreach(findMissingInPlace)
-      }
-    }
-    measure method "findMissingSet" in {
-      using(arrays) in {
-        arr => arr.foreach(findMissingSet)
-      }
-    }
+  /* Gen.single example
+      -> generates a single, specified value
+      -> must import org.scalameter.picklers.Implicits._
+   */
+
+  // every `using { ... }` block invokes a new JVM, so no need to `clone` arrays
+
+  performance of "Example #1: randArray(5, 0, 1)" in {
+
+    val arrayLength = Gen.single("n")(5)
+
+    val arrays = for (n <- arrayLength) yield randArray(n, 0, 1)
+
+    using(arrays) curve "findMissingMutable" in findMissingMutable
+    using(arrays) curve "findMissingInPlace" in findMissingInPlace
+    using(arrays) curve "findMissingSet" in findMissingSet
+  }
+
+  /* Gen.range example
+    -> generates a range of values with a min, max, and interval
+   */
+
+  performance of "Example #2: randArray(n, 0, 1) -- linear" in {
+
+    val arrayLength = Gen.range("n")(100, 1000, 100)
+
+    val arrays: Gen[Array[Int]] = for {
+      length <- arrayLength
+    } yield randArray(length, 0, 1)
+
+    using(arrays) curve "findMissingMutable" in { findMissingMutable }
+    using(arrays) curve "findMissingInPlace" in { findMissingInPlace }
+    using(arrays) curve "findMissingSet" in { findMissingSet }
+  }
+
+  /* Gen.exponential example
+    -> generates an exponentially-spaced range of values
+    -> uses a starting value and a multiplicative factor
+   */
+
+  performance of "Example #3: randArray(n, 0, 1) -- exponential" in {
+
+    val arrayLength = Gen.exponential("n")(2, Math.pow(2, 16).toInt, 2)
+
+    val arrays: Gen[Array[Int]] = for {
+      length <- arrayLength
+    } yield randArray(length, 0, 1)
+
+    using(arrays) curve "findMissingMutable" in { findMissingMutable }
+    using(arrays) curve "findMissingInPlace" in { findMissingInPlace }
+    using(arrays) curve "findMissingSet" in { findMissingSet }
+  }
+
+  performance of "Example #4: randArray(a, b, c)" in {
+
+    val arrayLength = Gen.exponential("n")(2, Math.pow(2, 16).toInt, 2)
+    val pctBad = Gen.range("fracBad")(0, 105, 5)
+    val pctScaling = Gen.range("maxScaling")(5, 305, 5)
+
+    val arrays: Gen[Array[Int]] = for {
+      length <- arrayLength
+      bad <- pctBad
+      scale <- pctScaling
+    } yield randArray(length, bad / 100.0, scale / 100.0)
+
+    using(arrays) curve "findMissingMutable" in { findMissingMutable }
+    using(arrays) curve "findMissingInPlace" in { findMissingInPlace }
+    using(arrays) curve "findMissingSet" in { findMissingSet }
   }
 
 }
